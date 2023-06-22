8d8f782ac4237d46775b4a616248f9f4af0bf081
zkwlr <130959672+zkwlr@users.noreply.github.com>
2023-06-23 05:11:01 +0900

    Revert "test"
    This reverts commit fad5e0e947405275778c3d044b0daf93bdfb4cbf.

--- assn4/PipelinedCPU.cpp
+++ assn4/PipelinedCPU.cpp
@@ -1,58 +1,53 @@
 #include "PipelinedCPU.hpp"
 
-void PipelinedCPU::InstructionFetch()
-{
+void PipelinedCPU::InstructionFetch() {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
 
-void PipelinedCPU::InstructionDecode()
-{
+void PipelinedCPU::InstructionDecode() {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
 
-void PipelinedCPU::Execute()
-{
+void PipelinedCPU::Execute() {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
 
-void PipelinedCPU::MemoryAccess()
-{
+void PipelinedCPU::MemoryAccess() {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
 
-void PipelinedCPU::WriteBack()
-{
+void PipelinedCPU::WriteBack() {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
-  // test
 }
 
 void PipelinedCPU::ForwardingUnit(
-    const std::bitset<5> *ID_EX_rs, const std::bitset<5> *ID_EX_rt,
-    const std::bitset<1> *EX_MEM_regWrite, const std::bitset<5> *EX_MEM_rd,
-    const std::bitset<1> *MEM_WB_regWrite, const std::bitset<5> *MEM_WB_rd,
-    std::bitset<2> *forwardA, std::bitset<2> *forwardB)
-{
+  const std::bitset<5> *ID_EX_rs, const std::bitset<5> *ID_EX_rt,
+  const std::bitset<1> *EX_MEM_regWrite, const std::bitset<5> *EX_MEM_rd,
+  const std::bitset<1> *MEM_WB_regWrite, const std::bitset<5> *MEM_WB_rd,
+  std::bitset<2> *forwardA, std::bitset<2> *forwardB
+) {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
 
 void PipelinedCPU::HazardDetectionUnit(
-    const std::bitset<5> *IF_ID_rs, const std::bitset<5> *IF_ID_rt,
-    const std::bitset<1> *ID_EX_memRead, const std::bitset<5> *ID_EX_rt,
-    std::bitset<1> *PCWrite, std::bitset<1> *IFIDWrite, std::bitset<1> *ctrlSelect)
-{
+  const std::bitset<5> *IF_ID_rs, const std::bitset<5> *IF_ID_rt,
+  const std::bitset<1> *ID_EX_memRead, const std::bitset<5> *ID_EX_rt,
+  std::bitset<1> *PCWrite, std::bitset<1> *IFIDWrite, std::bitset<1> *ctrlSelect
+) {
   /*****************************/
   /*********** FIXME ***********/
   /*****************************/
 }
+
